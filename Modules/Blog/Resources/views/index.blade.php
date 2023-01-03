73ba4f6a77e3dbb916dfb62903bc3306ddac1f28
Samuel Bellamacina <s.bellamacina@lswr.it>
2023-01-03 18:41:31 +0100

    Added italian translation for Hello World

--- Modules/Blog/Resources/views/index.blade.php
+++ Modules/Blog/Resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('blog::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+    <h1>{{ __('Hello World') }}</h1>
 
     <p>
         This view is loaded from module: {!! config('blog.name') !!}
